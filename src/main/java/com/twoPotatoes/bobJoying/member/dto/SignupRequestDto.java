9c6fbb9df47da03ef109a94134f29e7c4a1e01df
JisooPyo <audwl326@gmail.com>
2024-04-15 23:10:03 +0900

    update: SignupRequestDto `@builder` 추가

--- src/main/java/com/twoPotatoes/bobJoying/member/dto/SignupRequestDto.java
+++ src/main/java/com/twoPotatoes/bobJoying/member/dto/SignupRequestDto.java
@@ -1,15 +1,16 @@
 package com.twoPotatoes.bobJoying.member.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class SignupRequestDto {
     @Email(
         regexp = "^.+@.+\\..+$",
