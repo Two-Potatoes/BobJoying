f8f6a5704cca55a597d424ca6a7c74fb3de278aa
JisooPyo <audwl326@gmail.com>
2024-05-21 17:57:00 +0900

    chore: 필요없는 import문 제거

--- src/main/java/com/twoPotatoes/bobJoying/member/repository/RefreshTokenRepository.java
+++ src/main/java/com/twoPotatoes/bobJoying/member/repository/RefreshTokenRepository.java
@@ -1,7 +1,6 @@
 package com.twoPotatoes.bobJoying.member.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import com.twoPotatoes.bobJoying.member.entity.RefreshToken;
 
