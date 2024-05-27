c1e34cef0eada55e3a26c63ebdc21bee92144ed8
JisooPyo <audwl326@gmail.com>
2024-05-27 20:08:17 +0900

    test: 내 재료 등록 API 컨트롤러 테스트 작성

---
+++ src/test/java/com/twoPotatoes/bobJoying/ingredient/controller/MyIngredientControllerTest.java
@@ -0,0 +1,83 @@
+package com.twoPotatoes.bobJoying.ingredient.controller;
+
+import static com.twoPotatoes.bobJoying.ingredient.entity.StorageEnum.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.graphql.test.tester.GraphQlTester;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.twoPotatoes.bobJoying.common.config.GraphQlConfig;
+import com.twoPotatoes.bobJoying.common.dto.ApiResponseDto;
+import com.twoPotatoes.bobJoying.common.security.UserDetailsImpl;
+import com.twoPotatoes.bobJoying.ingredient.dto.MyIngredientCreateRequestDto;
+import com.twoPotatoes.bobJoying.ingredient.entity.StorageEnum;
+import com.twoPotatoes.bobJoying.ingredient.service.MyIngredientService;
+import com.twoPotatoes.bobJoying.member.entity.Member;
+import com.twoPotatoes.bobJoying.member.entity.MemberRoleEnum;
+
+@GraphQlTest(MyIngredientController.class)
+@Import(GraphQlConfig.class)
+class MyIngredientControllerTest {
+    @Autowired
+    private GraphQlTester graphQlTester;
+
+    @MockBean
+    private MyIngredientService myIngredientService;
+    UserDetailsImpl userDetails;
+    ApiResponseDto apiResponseDto;
+
+    @BeforeEach
+    void setUp() {
+        Member member = Member.builder().role(MemberRoleEnum.MEMBER).build();
+        userDetails = new UserDetailsImpl(member);
+        SecurityContextHolder
+            .getContext()
+            .setAuthentication(
+                new UsernamePasswordAuthenticationToken(
+                    userDetails, null, userDetails.getAuthorities()
+                )
+            );
+        apiResponseDto = new ApiResponseDto("요청 성공!");
+    }
+
+    @Test
+    @DisplayName("MyIngredientController Test - createMyIngredient")
+    void createMyIngredient() {
+        // given
+        Map<String, Object> createRequestDto = new HashMap<>();
+
+        createRequestDto.put("ingredientId", 1);
+        createRequestDto.put("quantity", 1.5f);
+        createRequestDto.put("unit", "개");
+        createRequestDto.put("storageDate", "2024-05-17");
+        createRequestDto.put("expirationDate", "2024-05-27");
+        createRequestDto.put("storage", FRIDGE);
+
+        given(myIngredientService.createMyIngredient(
+                any(UserDetailsImpl.class),
+                any(MyIngredientCreateRequestDto.class)
+            )
+        ).willReturn(apiResponseDto);
+
+        // when
+        graphQlTester.documentName("myIngredient")
+            .variable("input", createRequestDto)
+            .operationName("createMyIngredient")
+            .execute()
+            .path("createMyIngredient.message")
+            .entity(String.class);
+    }
+}
